e310ca5a1888be4fd753f8d18f7ef98b954b4d3f
Andreeva Nina <nandreewaa@gmail.com>
2023-01-23 14:37:29 +0300

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args)  {
+        String host ="127.0.0.1";
+        int port = 8080;
+        System.out.println(host);
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ){
+
+            out.println("Netology");
+
+            String resp = in.readLine();
+            System.out.println(resp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
